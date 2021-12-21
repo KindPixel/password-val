086313bf19edb6feaf1615b6dc2c16c76f789d10
KindPixel <50356708+KindPixel@users.noreply.github.com>
2021-12-21 11:32:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-/*Actual validation function*/
+
 function ValidatePassword() {
-    /*Array of rules and the information target*/
+
     var rules = [{
         Pattern: "[A-Z]",
         Target: "UpperCase"
@@ -19,16 +19,14 @@ function ValidatePassword() {
     }
     ];
 
-    //Just grab the password once
+
     var password = $(this).val();
 
-    /*Length Check, add and remove class could be chained*/
-    /*I've left them seperate here so you can see what is going on */
-    /*Note the Ternary operators ? : to select the classes*/
+   
     $("#Length").removeClass(password.length > 6 ? "glyphicon-remove" : "glyphicon-ok");
     $("#Length").addClass(password.length > 6 ? "glyphicon-ok" : "glyphicon-remove");
 
-    /*Iterate our remaining rules. The logic is the same as for Length*/
+  
     for (var i = 0; i < rules.length; i++) {
 
         $("#" + rules[i].Target).removeClass(new RegExp(rules[i].Pattern).test(password) ? "glyphicon-remove" : "glyphicon-ok");
@@ -36,7 +34,7 @@ function ValidatePassword() {
     }
 }
 
-/*Bind our event to key up for the field. It doesn't matter if it's delete or not*/
+
 $(document).ready(function () {
     $("#NewPassword").on('keyup', ValidatePassword)
 });
